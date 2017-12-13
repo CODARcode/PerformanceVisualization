820ed5e0a2993cd977fb3da9f48cfb2c2038e05b
Wei Xu <xuw@bnl.gov>
2017-12-13 17:43:19 -0500

    Update import.sh

--- preprocess/import.sh
+++ preprocess/import.sh
@@ -3,3 +3,5 @@ do
 	echo $i
 	mongoimport --db codarvis --collection trace_events --file "trace."$i".json" --jsonArray
 done
+
+mongoimport --db codarvis --collection summary --file "summary.json" --jsonArray
